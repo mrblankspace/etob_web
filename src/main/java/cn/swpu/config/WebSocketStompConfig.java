995d86aed3d7396b0e08ed574042374bed0aa537
BlankSpace <1042079408@qq.com>
2018-07-25 19:52:40 +0800

    find message

--- src/main/java/cn/swpu/config/WebSocketStompConfig.java
+++ src/main/java/cn/swpu/config/WebSocketStompConfig.java
@@ -16,9 +16,9 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 public class WebSocketStompConfig  extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        //registry.enableSimpleBroker("/")
+        registry.enableSimpleBroker("/queue","/topic");
         registry.setApplicationDestinationPrefixes("/app");
-    }
+}
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
--- src/main/java/cn/swpu/web/StompMessageAction.java
+++ src/main/java/cn/swpu/web/StompMessageAction.java
@@ -1,9 +1,11 @@
 package cn.swpu.web;
 
+import cn.swpu.beans.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.messaging.Message;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.WebSocketSession;
 
@@ -15,9 +17,10 @@ import org.springframework.web.socket.WebSocketSession;
 public class StompMessageAction {
     private static final Logger logger = LoggerFactory.getLogger(StompMessageAction.class);
 
-    @MessageMapping("/api/charRoom/getStompMessage")
-    public Message sendMessage( Message message){
-        System.out.println(message);
+    @MessageMapping("/api/charRoom/getNewMessage")    //注意隐含的  /app 前缀   请求要带/app
+    @SendTo("/topic/chatRoom")   //重載发布消息地址
+    public Message sendMessage(Message message){
         return message;
     }
+
 }
